75b511abed112882ef89d7ddd40f85da33b3996f
doanh <nguyentrongdoanh202@gmail.com>
2024-11-03 23:59:21 +0700

    update src code

--- frontend/src/views/admin/ManagePayments.vue
+++ frontend/src/views/admin/ManagePayments.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
-    <div
-      style="display: flex; justify-content: space-between; align-items: center"
-    >
+    <div style="display: flex; justify-content: space-between; align-items: center;">
       <h1>Manage Payments</h1>
 
       <!-- Search bar -->
       <el-input
-        v-model="searchQuery"
-        placeholder="Search by Email or Payment Details"
-        style="width: 300px"
-        @input="filterPayments"
+          v-model="searchQuery"
+          placeholder="Search by Email or Payment Details"
+          style="width: 300px;"
+          @input="filterPayments"
       >
         <template #prepend>
           <el-button :icon="Search" />
@@ -44,7 +42,9 @@
 <script setup>
 import { onMounted, ref, computed } from 'vue';
 import { ElMessage } from 'element-plus';
-import { getAllPayments } from '@/services/admin/admin-payment-service';
+import {
+  getAllPayments,
+} from '@/services/admin/admin-payment-service';
 import { Search } from '@element-plus/icons-vue';
 import { format } from 'date-fns';
 
@@ -54,8 +54,7 @@ const searchQuery = ref('');
 // Filter payments based on search query
 const filteredPayments = computed(() => {
   const query = searchQuery.value.toLowerCase();
-  return payments.value.filter(
-    (payment) =>
+  return payments.value.filter(payment =>
       payment.amount.toString().includes(query) ||
       payment.date.toLowerCase().includes(query)
   );
